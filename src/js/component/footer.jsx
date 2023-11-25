5c5c83648ca5da3e37625096a51cea45965abbee
Noe CM <128952542+NoeAV1314@users.noreply.github.com>
2023-11-25 01:35:23 +0000

    No se porque no hace scroll T-T

--- src/js/component/footer.jsx
+++ src/js/component/footer.jsx
@@ -3,7 +3,7 @@ import React from "react";
 //create your first component
 const Footer = () => {
      return (
-       <div className="footer" style={{ backgroundColor: "black", color: "white", textAlign: "center", padding: "10px", position: "fixed", bottom: "0", width: "100%" }}>
+       <div className="footer" style={{ backgroundColor: "black", color: "white", textAlign: "center", padding: "10px", width: "100%" }}>
          <cite>2023 Your Website. All rights reserved.</cite>
        </div>
    );
--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 
 //include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
 import Navbar from "./navbar";
 import Cards from "./cards";
 import Footer from "./footer";
@@ -10,7 +9,7 @@ import Jumbotron from "./jumbotron";
 //create your first component
 const Home = () => {
 	return (
-		<div className="Proyect" style={{scrollBehavior: "smooth"}}>
+		<div className="Proyect" style={{scrollBehavior: "smooth",  overflowY:"auto"}}>
 			<Navbar/>
 			{/* Aqui empieza el div para el body y las cartas. */}
 
@@ -18,11 +17,7 @@ const Home = () => {
 				{/* Jumbotron model */}
 				<div className="jumbotron">
 					<Jumbotron/>
-					<hr className="my-4">
-						{/* <p className="lead">
-						<a className="btn btn-primary" href="#" role="button">Learn more</a>
-						</p> */}
-					</hr>
+					<hr className="my-4"></hr>
 				</div>
 				{/* Aquí empiezan las cartas */}
 					<div className="d-flex"><Cards/></div>
